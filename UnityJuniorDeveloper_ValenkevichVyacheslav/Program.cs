78775a6c9903499d36533c3a9a767a5e8028d393
coldborne <slava2232162012@gmail.com>
2022-08-14 23:10:07 +0300

    Задание "variables"
    Создал класс "Variables", в котором объявил и проинициализировал 10 переменных разного типа

--- UnityJuniorDeveloper_ValenkevichVyacheslav/Program.cs
+++ UnityJuniorDeveloper_ValenkevichVyacheslav/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UnityJuniorDeveloper_ValenkevichVyacheslav
+﻿namespace UnityJuniorDeveloper_ValenkevichVyacheslav
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
---
+++ UnityJuniorDeveloper_ValenkevichVyacheslav/Variables.cs
@@ -0,0 +1,16 @@
+﻿namespace UnityJuniorDeveloper_ValenkevichVyacheslav
+{
+    internal class Variables
+    {
+        bool isTimeForDinner = true;
+        bool[] haveComputers = null;
+        int cookiesCount = 10;
+        int[] childhoodTraumaCount = { 5, 4, 3 };
+        byte[] sizeOfDataTypes = null;
+        char favoriteLetter = 'J';
+        string[] phrasesOfGreatPeople = { "Я великий", "Я тоже великий", "А я C# разработчик" };
+        float[] landАreaInMillions = { 43.4f, 30.3f, 24.71f, 17.84f, 14.1f, 10f, 7.66f };
+        byte intSize = 4;
+        short swimmingDistance = 100;
+    }
+}
