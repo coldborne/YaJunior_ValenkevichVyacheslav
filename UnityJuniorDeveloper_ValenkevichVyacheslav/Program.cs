27ce46ed220a7e442a54f8275de95afcbaf5ce6b
coldborne <slava2232162012@gmail.com>
2022-08-14 22:33:04 +0300

    Add project files.

---
+++ UnityJuniorDeveloper_ValenkevichVyacheslav/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityJuniorDeveloper_ValenkevichVyacheslav
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
