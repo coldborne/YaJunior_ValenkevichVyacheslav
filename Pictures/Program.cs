d904c1062345e27a593805b4a4d213f3c4ceb97d
coldborne <slava2232162012@gmail.com>
2022-08-15 21:44:21 +0300

    Задача "Картинки"
    Добавил новый проект Pictures, внутри него в классе "Program" в методе Main реализовал подсчёт количества заполненных рядов картинок + количества картинок в оставшемся незаполненном ряду

---
+++ Pictures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesCount = 52;
+            int picturesInRow = 3;
+
+            int filledRows = picturesCount / picturesInRow;
+            int picturesInIncompleteRow = picturesCount % picturesInRow;
+
+            Console.WriteLine("Количество заполненных рядов");
+            Console.WriteLine(filledRows);
+
+            Console.WriteLine("Количество картинок в незаполненном ряду");
+            Console.WriteLine(picturesInIncompleteRow);
+        }
+    }
+}
