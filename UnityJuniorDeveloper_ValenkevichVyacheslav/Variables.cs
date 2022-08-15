fb4ca3bcfaeb92a1edbda33e682461e669ba5e4a
coldborne <slava2232162012@gmail.com>
2022-08-15 20:24:02 +0300

    Задача "Переменные" - v3
    Убрал массивы, заменил их на другие примитивные типы

--- UnityJuniorDeveloper_ValenkevichVyacheslav/Variables.cs
+++ UnityJuniorDeveloper_ValenkevichVyacheslav/Variables.cs
@@ -5,13 +5,13 @@
         private static void InitVariables()
         {
             bool isTimeForDinner = true;
-            bool[] haveComputers = null;
+            sbyte roomTemperature = -4;
             int cookiesCount = 10;
-            int[] childhoodTraumaCount = { 5, 4, 3 };
-            byte[] sizeOfDataTypes = null;
+            ushort dishesInStock = 1000;
+            float asiaSquare = 44.58E7F;
             char favoriteLetter = 'J';
-            string[] phrasesOfGreatPeople = { "Я великий", "Я тоже великий", "А я C# разработчик" };
-            float[] landАreaInMillions = { 43.4f, 30.3f, 24.71f, 17.84f, 14.1f, 10f, 7.66f };
+            string secretCode = "Тима";
+            decimal starsCount = 10.5E10M;
             byte intSize = 4;
             short swimmingDistance = 100;
         } 
