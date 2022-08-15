d48c6c6ce79b9b9302e242c326dcf09f759264a1
coldborne <slava2232162012@gmail.com>
2022-08-15 20:03:05 +0300

    Задача "Переменные" - v2
    Создал метод InitVariables, чтобы объявить в нём переменные, и они перестали быть полями класса

--- UnityJuniorDeveloper_ValenkevichVyacheslav/Variables.cs
+++ UnityJuniorDeveloper_ValenkevichVyacheslav/Variables.cs
@@ -2,15 +2,18 @@
 {
     internal class Variables
     {
-        bool isTimeForDinner = true;
-        bool[] haveComputers = null;
-        int cookiesCount = 10;
-        int[] childhoodTraumaCount = { 5, 4, 3 };
-        byte[] sizeOfDataTypes = null;
-        char favoriteLetter = 'J';
-        string[] phrasesOfGreatPeople = { "Я великий", "Я тоже великий", "А я C# разработчик" };
-        float[] landАreaInMillions = { 43.4f, 30.3f, 24.71f, 17.84f, 14.1f, 10f, 7.66f };
-        byte intSize = 4;
-        short swimmingDistance = 100;
+        private static void InitVariables()
+        {
+            bool isTimeForDinner = true;
+            bool[] haveComputers = null;
+            int cookiesCount = 10;
+            int[] childhoodTraumaCount = { 5, 4, 3 };
+            byte[] sizeOfDataTypes = null;
+            char favoriteLetter = 'J';
+            string[] phrasesOfGreatPeople = { "Я великий", "Я тоже великий", "А я C# разработчик" };
+            float[] landАreaInMillions = { 43.4f, 30.3f, 24.71f, 17.84f, 14.1f, 10f, 7.66f };
+            byte intSize = 4;
+            short swimmingDistance = 100;
+        } 
     }
 }
