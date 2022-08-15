b4414e9402ae19b9533862c13c80e2fa45b71697
coldborne <slava2232162012@gmail.com>
2022-08-16 00:07:31 +0300

    Задача "Перестановка местами значений"
    Реализовал перестановку значений переменных surname и name, также вывод в консоль данных до перестановки и после

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwappingValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String surname = "Slavka";
+            String name = "Valenkevich";
+
+            Console.WriteLine($"Фамилия: {surname}\nИмя: {name}\n");
+
+            String temporaryString = surname;
+            surname = name;
+            name = temporaryString;
+
+            Console.WriteLine($"Фамилия: {surname}\nИмя: {name}\n");
+        }
+    }
+}
