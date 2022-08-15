42057182bfb57c884904645c164c5ec935773d28
coldborne <slava2232162012@gmail.com>
2022-08-15 20:35:45 +0300

    Задача "Работа со строками"
    В методе Main прописал считывание с клавиатуры возраста, имя, места работы и любимого блюда пользователя. Затем прописал вывод полученной от пользователя информации в консоль

--- UnityJuniorDeveloper_ValenkevichVyacheslav/Program.cs
+++ UnityJuniorDeveloper_ValenkevichVyacheslav/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace UnityJuniorDeveloper_ValenkevichVyacheslav
+﻿using System;
+
+namespace ProjectForDemoCSharp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите свой возраст");
+            int.TryParse(Console.ReadLine(), out int age);
+
+            Console.WriteLine("Введите своё имя");
+            string name = Console.ReadLine().Replace(" ", "");
+
+            Console.WriteLine("Введите своё место работы");
+            string workPlace = Console.ReadLine().Replace(" ", "");
+
+            Console.WriteLine("Введите своё любимое блюдо");
+            string favoriteDish = Console.ReadLine().Replace(" ", "");
+
+            Console.WriteLine($"Ваше имя: {name}.\nВаш возраст: {age}.\nВаше место работы: {workPlace}.\nВаше любимое блюдо: {favoriteDish}.");
         }
     }
 }
