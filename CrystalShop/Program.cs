c86dbad25ed0cd1283f0814382285ea9a8bc8ea6
coldborne <slava2232162012@gmail.com>
2022-08-17 00:54:28 +0300

    Задача "Магазин кристаллов"
    Реализовал покупку кристаллов за золото, а также вывод итогого количества кристаллов и золота

---
+++ CrystalShop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CrystalShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldForOneCrystal = 4;
+            int crystalAmount = 0;
+
+            Console.WriteLine("Введите количества Вашего золота");
+            bool isIntValue = int.TryParse(Console.ReadLine(), out int goldAmount);
+
+            if (!isIntValue)
+            {
+                Console.WriteLine("Можно вводить только числа");
+            }
+
+            crystalAmount = goldAmount / goldForOneCrystal;
+            goldAmount -= crystalAmount * goldForOneCrystal;
+
+            Console.WriteLine($"Куплено кристаллов - {crystalAmount}\nУ Вас осталось золота - {goldAmount}");
+
+        }
+    }
+}
