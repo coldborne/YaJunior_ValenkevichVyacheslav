126ec0c3e08e03026c86fd317658c37ec974514c
coldborne <slava2232162012@gmail.com>
2022-08-17 01:12:32 +0300

    Задача "Поликлиника"
    Реализовал подсчёт времени ожидания, в зависимости от количества людей в очереди

---
+++ QueueAtClinic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAtClinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int appointmentMinutesPerPerson = 10;
+            int minutesInHour = 60;
+
+            Console.WriteLine("Введите количество существ, которых Вы терпеть не можете, в очереди");
+            bool isIntValue = int.TryParse(Console.ReadLine(), out int peopleInQueue);
+
+            if (!isIntValue)
+            {
+                Console.WriteLine("Можно вводить только числа");
+            }
+
+            int minutesWaitingInQueue = peopleInQueue * appointmentMinutesPerPerson;
+            int hoursWaitingInQueue = minutesWaitingInQueue / minutesInHour;
+
+            Console.WriteLine("Ваше время ожидания в очереди - " + hoursWaitingInQueue.ToString() + " часов " + minutesWaitingInQueue.ToString() + " минут");
+        }
+    }
+}
