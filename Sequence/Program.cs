2dac816441ecb24382ee8e7805da868bb04c6823
coldborne <slava2232162012@gmail.com>
2022-08-18 20:44:11 +0300

    Задача "Последовательность"
    Реализовал вывод последовательности в консоль с помощью цикла while

---
+++ Sequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentNumber = 5;
+            int endNumber = 96;
+
+            //Цикл while выбран, так как нам неважно, сколько раз выполнится цикл, нам лишь нужно показать условие выхода из цикла
+            while(currentNumber <= endNumber)
+            {
+                Console.WriteLine(currentNumber);
+                currentNumber+=7;
+            }
+        }
+    }
+}
