d806c8d0334ac34f128c8a9f7452582acd248310
coldborne <slava2232162012@gmail.com>
2022-08-18 19:41:24 +0300

    Задание "Освоение массивов"
    Добавил возможность выводить в цикле сообщение, которое ввёл пользователь, столько раз, сколько скажет пользователь

---
+++ MasteringCycles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MasteringCycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое сообщение вывести?");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("Сколько раз вывести Ваше сообщение?");
+            int numberOfMessages = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numberOfMessages; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
