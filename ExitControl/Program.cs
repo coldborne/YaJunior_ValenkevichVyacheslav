0f42a6653e65ab8a4e750d300e30e2d47a7cc71c
coldborne <slava2232162012@gmail.com>
2022-08-18 20:07:59 +0300

    Задача "Контроль выхода"
    Добавил возможность пользователю вводить сообщения, пока он не введёт слово "exit", причем регистр неважен

---
+++ ExitControl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExitControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageToComplete = "exit";
+            bool enteredCorrectMessage = false;
+
+            Console.WriteLine("Чтобы завершить программу, нужно ввести \"exit\"");
+
+            while(enteredCorrectMessage == false)
+            {
+                Console.WriteLine("Введите сообщение");
+                string userMessage = Console.ReadLine().ToLower();
+
+                if(userMessage == messageToComplete)
+                {
+                    Console.WriteLine("Завершаем программу");
+                    enteredCorrectMessage = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("По такому сообщению программа не завершится");
+                    continue;
+                }
+            }
+        }
+    }
+}
