2d507792f86d7d589b5dffab6b2e1af51e4a5ff5
coldborne <slava2232162012@gmail.com>
2022-08-18 21:05:07 +0300

    Задача "Сумма чисел"
    Реализовал подсчёт суммы чисел кратных 3 и 5 в диапозоне от 1 до рандомного числа, которое не больше 100

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int randomNumber = random.Next(101);
+            int sumOfNumbers = 0;
+
+            Console.WriteLine($"Вам выпало число - {randomNumber}");
+
+            for (int currentNumber = 1; currentNumber <= randomNumber; currentNumber++)
+            {
+                if (currentNumber % 3 == 0 || currentNumber % 5 == 0)
+                {
+                    sumOfNumbers += currentNumber;
+                }
+            }
+
+            Console.WriteLine($"Сумма чисел кратных 3 и 5 в диапозоне от 1 до {randomNumber} = {sumOfNumbers}");
+        }
+    }
+}
