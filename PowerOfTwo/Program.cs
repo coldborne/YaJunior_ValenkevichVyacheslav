5eb20600002b6bafdc9d8bc7434c8da944da1206
coldborne <slava2232162012@gmail.com>
2022-08-20 02:42:06 +0300

    Задача "Степень двойки"
    Реализовал нахождение ближайшего бОльшего числа, у которого степень двойки больше, чем у текущего

---
+++ PowerOfTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maximumRandomNumberLimit = 100;
+            int number = random.Next(maximumRandomNumberLimit+1);
+
+            int powerOfTwo = 0;
+
+            Console.WriteLine("Число - " + number);
+
+            int numberCopy = number;
+
+            while(numberCopy > 0)
+            {
+                numberCopy /= 2;
+                powerOfTwo++;
+            }
+
+            int nearestHigherNumber = Convert.ToInt32(Math.Pow(2, powerOfTwo));
+            Console.WriteLine("Степень двойки превосходящего числа - " + powerOfTwo);
+            Console.WriteLine("Превосходящее число " + nearestHigherNumber);
+        }
+    }
+}
