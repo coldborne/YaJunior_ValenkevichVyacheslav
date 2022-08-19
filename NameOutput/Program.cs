b7b4d20fded423480963d0b5245f7fe9f81f7956
coldborne <slava2232162012@gmail.com>
2022-08-20 02:14:01 +0300

    Задача "Вывод имени"
    Реализовал вывод введенного имени в рамке из выбранного символа

---
+++ NameOutput/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите символ для рамки");
+            char symbolForFrame = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            int nameLength = name.Length;
+            int stringsInFrameAmount = 3;
+            int columnsInFrameAmount = nameLength + 2;
+
+            for (int i = 0; i < stringsInFrameAmount; i++)
+            {
+                if (i == 0 || i == stringsInFrameAmount - 1)
+                {
+                    for (int j = 0; j < columnsInFrameAmount; j++)
+                    {
+                        Console.Write(symbolForFrame);
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(symbolForFrame + name + symbolForFrame);
+                }
+            }
+        }
+    }
+}
