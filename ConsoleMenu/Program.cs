086cfb0ed11ea44636827616498377a48a463e9a
coldborne <slava2232162012@gmail.com>
2022-08-20 00:49:57 +0300

    Задача "Консольное меню"
    Реализовал 4 метода, которые может исполнять программа

---
+++ ConsoleMenu/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace ConsoleMenu
+{
+    internal class Program
+    {
+        private const string firstCommand = "PredictYearOfPurchaseApartment";
+        private const string secondtCommand = "CompareNumbers";
+        private const string thirdCommand = "ChangeConsoleSize";
+        private const string fourthCommand = "Esc";
+
+        private static bool _isExitCommandChosen = false;
+
+        private static bool _isYearPredicted = false;
+        private static int _predictedYearOfPurchaseApartment;
+
+        static void Main(string[] args)
+        {
+            ListCommands();
+
+            while (_isExitCommandChosen == false)
+            {
+                string chosenTeam = GetChosenCommand();
+                ChooseCommand(chosenTeam);
+            }
+        }
+
+        private static string GetChosenCommand()
+        {
+            return ReadStringValueFromKeyboard();
+        }
+
+        private static void ChooseCommand(string chosenTeam)
+        {
+            switch (chosenTeam)
+            {
+                case firstCommand:
+                    if (_isYearPredicted == false)
+                    {
+                        PredictYearOfPurchaseApartment();
+                    }
+
+                    Console.WriteLine(_predictedYearOfPurchaseApartment);
+                    break;
+                case secondtCommand:
+                    Console.WriteLine("Введите первое число");
+                    int firstNumber = ReadIntValueFromKeyboard();
+                    Console.WriteLine("Введите второе число");
+                    int secondNumber = ReadIntValueFromKeyboard();
+
+                    CompareNumbers(firstNumber, secondNumber);
+                    break;
+                case thirdCommand:
+                    Console.WriteLine("Текущий размер консоли: " + Console.WindowWidth + " " + Console.WindowHeight);
+                    Console.WriteLine("Изменить можно только на размер меньше, чем текущий");
+
+                    Console.WriteLine("Введите желаемую ширину консоли");
+                    int newConsoleWidth = ReadIntValueFromKeyboard();
+                    Console.WriteLine("Введите желаемую высоту консоли");
+                    int newConsoleheight = ReadIntValueFromKeyboard();
+
+                    ChangeConsoleSize(newConsoleWidth, newConsoleheight);
+                    break;
+                case fourthCommand:
+                    EndProgram();
+                    break;
+                default:
+                    Console.WriteLine("Такое Мы не умеем, выберите что-то, что Мы можем");
+                    break;
+            }
+        }
+
+        private static string ReadStringValueFromKeyboard()
+        {
+            string valueFromKeyboard = Console.ReadLine().Trim();
+            if (valueFromKeyboard == "")
+            {
+                Console.WriteLine("Программа не может состоять из пустой строки");
+                return ReadStringValueFromKeyboard();
+            }
+            else
+            {
+                return valueFromKeyboard;
+            }
+        }
+
+        private static int ReadIntValueFromKeyboard()
+        {
+            bool isIntValue = int.TryParse(Console.ReadLine(), out int value);
+
+            if (isIntValue == true)
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Можно вводить только положительные числа");
+                    return ReadIntValueFromKeyboard();
+                }
+                else
+                {
+                    return value;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Можно вводить только числа");
+                return ReadIntValueFromKeyboard();
+            }
+        }
+
+        private static void ListCommands()
+        {
+            Console.WriteLine("Наша программа умеет выполнять следующие команды:");
+            Console.WriteLine($"{firstCommand} - предсказать год, когда Вы купите квартиру");
+            Console.WriteLine($"{secondtCommand} - сравнить два числа");
+            Console.WriteLine($"{thirdCommand} - изменить размер консоли");
+            Console.WriteLine($"{fourthCommand} - выход из программы");
+        }
+
+        private static void PredictYearOfPurchaseApartment()
+        {
+            Random random = new Random();
+
+            int currentYear = DateTime.Now.Year;
+
+            _predictedYearOfPurchaseApartment = random.Next(currentYear, currentYear + 50);
+            _isYearPredicted = true;
+        }
+
+        private static void CompareNumbers(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine($"{firstNumber} = {secondNumber}");
+            }
+            else if (firstNumber > secondNumber)
+            {
+                Console.WriteLine($"{firstNumber} > {secondNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{firstNumber} < {secondNumber}");
+            }
+        }
+
+        private static void ChangeConsoleSize(int newConsoleWidth, int newConsoleheight)
+        {
+            int currentWidth = Console.WindowWidth;
+            int currentHeight = Console.WindowHeight;
+
+            if (newConsoleWidth > currentWidth || newConsoleheight > currentHeight)
+            {
+                Console.WriteLine("Невозможно поменять размер консоли на бОльший, чем текущий");
+            }
+            else
+            {
+                Console.SetWindowSize(newConsoleWidth, newConsoleheight);
+            }
+        }
+
+        private static void EndProgram()
+        {
+            _isExitCommandChosen = true;
+        }
+    }
+}
