0bbcc2aa0d364bc18810381a1d1ae577f9f06669
coldborne <slava2232162012@gmail.com>
2022-08-20 02:45:07 +0300

    Поправил пробелы

--- PowerOfTwo/Program.cs
+++ PowerOfTwo/Program.cs
@@ -8,21 +8,21 @@ namespace PowerOfTwo
         {
             Random random = new Random();
             int maximumRandomNumberLimit = 100;
-            int number = random.Next(maximumRandomNumberLimit+1);
-
-            int powerOfTwo = 0;
+            int number = random.Next(maximumRandomNumberLimit + 1);
 
             Console.WriteLine("Число - " + number);
 
             int numberCopy = number;
+            int powerOfTwo = 0;
 
-            while(numberCopy > 0)
+            while (numberCopy > 0)
             {
                 numberCopy /= 2;
                 powerOfTwo++;
             }
 
             int nearestHigherNumber = Convert.ToInt32(Math.Pow(2, powerOfTwo));
+
             Console.WriteLine("Степень двойки превосходящего числа - " + powerOfTwo);
             Console.WriteLine("Превосходящее число " + nearestHigherNumber);
         }
