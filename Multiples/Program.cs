a9c935ea8c61801c5d3bcbfa28c61ca8219977af
coldborne <slava2232162012@gmail.com>
2022-08-21 19:16:30 +0300

    Задача "Кратные числа"
    Реализовал поиск чисел от 100 до 999, которые кратны числу N

---
+++ Multiples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maximumForMultiplier = 27;
+            int multiplier = random.Next(maximumForMultiplier) + 1;
+
+            Console.WriteLine($"Число N = {multiplier}");
+
+            int lowerLimit = 100;
+            int upperLimit = 999;
+            int multiplesOfMultiplierCount = 0;
+
+            int temporaryVariable = multiplier;
+            while (temporaryVariable < lowerLimit)
+            {
+                temporaryVariable += multiplier;
+            }
+
+            for (; temporaryVariable < upperLimit; temporaryVariable += multiplier)
+            {
+                multiplesOfMultiplierCount++;
+            }
+
+            Console.WriteLine($"Количество чисел, кратных {multiplier} в диапозоне от {lowerLimit} до {upperLimit} = {multiplesOfMultiplierCount}");
+        }
+    }
+}
