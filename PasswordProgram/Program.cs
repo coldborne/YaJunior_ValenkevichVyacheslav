787365af8e88ebbc685711ae39dc9adfd6cd1a07
coldborne <slava2232162012@gmail.com>
2022-08-21 15:52:36 +0300

    Задача "Программа под паролем"
    Реализовал возможность ввести пароль, если он верный, то выведется секретное сообщение. Если пароль не  угадывается 3 раза, то программа завершается

---
+++ PasswordProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PasswordProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int attemptsCount = 3;
+            string correctPassword = "JoJo";
+            string secretMessage = "It`s me - DIO";
+
+            Console.WriteLine("Введите пароль");
+
+            while (attemptsCount > 0)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput == correctPassword)
+                {
+                    Console.WriteLine("Верно");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно");
+                    attemptsCount--;
+                }
+            }
+
+            if (attemptsCount > 0)
+            {
+                Console.WriteLine(secretMessage);
+            }
+            else
+            {
+                Console.WriteLine("Увы, попробуйте в другой раз");
+            }
+        }
+    }
+}
