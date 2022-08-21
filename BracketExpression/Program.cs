093737650aa67862caf28b94a84ebfb4960c280a
coldborne <slava2232162012@gmail.com>
2022-08-21 16:49:05 +0300

    Задача "Скобочное выражение"
    Реализовал возможность проверки валидности скобочного выражения с точным определением глубины, в случае, если выражение верное

---
+++ BracketExpression/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BracketExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int expressionValidityScore = 0;
+            int depth = 0;
+            char closingBracket = ')';
+            char openingBracket = '(';
+            string stringOfBrackets = "(()()())";
+
+            foreach (var symbol in stringOfBrackets)
+            {
+                if (symbol == openingBracket)
+                {
+                    expressionValidityScore++;
+
+                    if (expressionValidityScore > depth)
+                    {
+                        depth = expressionValidityScore;
+                    }
+                }
+                else if (symbol == closingBracket)
+                {
+                    expressionValidityScore--;
+                }
+            }
+
+            Console.WriteLine(stringOfBrackets);
+
+            if (expressionValidityScore == 0)
+            {
+                Console.WriteLine("Строка верная, глубина = " + depth);
+            }
+            else
+            {
+                Console.WriteLine("Строка неверная");
+            }
+        }
+    }
+}
