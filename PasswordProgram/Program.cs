28eecd5e7b65704116bf815c4ccc56317bb8a96d
coldborne <slava2232162012@gmail.com>
2022-08-21 21:31:23 +0300

    Задача "Программа с паролем"
    Переделал цикл, в котором пользователь пытается ввести пароль

--- PasswordProgram/Program.cs
+++ PasswordProgram/Program.cs
@@ -7,31 +7,30 @@ namespace PasswordProgram
         static void Main(string[] args)
         {
             int attemptsCount = 3;
+            int remainingAttempts = attemptsCount;
             string correctPassword = "JoJo";
             string secretMessage = "It`s me - DIO";
 
             Console.WriteLine("Введите пароль");
 
-            while (attemptsCount > 0)
+            for (int i = attemptsCount; i > 0; i--)
             {
                 string userInput = Console.ReadLine();
+
                 if (userInput == correctPassword)
                 {
                     Console.WriteLine("Верно");
-                    break;
+                    Console.WriteLine("Секретное сообщение - " + secretMessage);
+                    i = 0;
                 }
                 else
                 {
+                    remainingAttempts--;
                     Console.WriteLine("Неверно");
-                    attemptsCount--;
                 }
             }
 
-            if (attemptsCount > 0)
-            {
-                Console.WriteLine(secretMessage);
-            }
-            else
+            if (remainingAttempts == 0)
             {
                 Console.WriteLine("Увы, попробуйте в другой раз");
             }
