7a8c33775ea7d76ce31ba848394d0471dd30b87a
coldborne <slava2232162014@gmail.com>
2022-08-26 00:31:55 +0300

    Задача "Локальные максимумы" - v2
    Сделал вычисление длины везде в циклах
    Переделал высчитывание максимумов, теперь цикл проходит по каждому элементу, и если происходит ситуация аля "8 8 3", то 8 по середине - это локальный максимум тоже
    И если все числа, например, 5, то все числа будут локальными максимумами

--- LocalMaxima/Program.cs
+++ LocalMaxima/Program.cs
@@ -12,41 +12,39 @@ namespace LocalMaxima
             Random random = new Random();
             int maximumValue = 100;
 
-            for (int i = 0; i < StudentGradesSize; i++)
+            for (int i = 0; i < studentGrades.Length; i++)
             {
                 studentGrades[i] = random.Next(0, maximumValue + 1);
             }
 
             Console.WriteLine("Ваш массив:");
 
-            for (int i = 0; i < StudentGradesSize; i++)
+            for (int i = 0; i < studentGrades.Length; i++)
             {
                 Console.Write(studentGrades[i] + " ");
             }
 
             Console.WriteLine("\nВсе локальные максимумы:");
 
-            for (int i = 0; i < StudentGradesSize; i++)
+            for (int i = 0; i < studentGrades.Length; i++)
             {
-                if (i > 0 && i < (StudentGradesSize - 1))
+                if (i > 0 && i < (studentGrades.Length - 1))
                 {
-                    if (studentGrades[i] > studentGrades[i - 1] && studentGrades[i] > studentGrades[i + 1])
+                    if (studentGrades[i] >= studentGrades[i - 1] && studentGrades[i] >= studentGrades[i + 1])
                     {
                         Console.Write(studentGrades[i] + " ");
-                        i++;
                     }
                 }
                 else if (i == 0)
                 {
-                    if (studentGrades[i] > studentGrades[i + 1])
+                    if (studentGrades[i] >= studentGrades[i + 1])
                     {
                         Console.Write(studentGrades[i] + " ");
-                        i++;
                     }
                 }
-                else if (i == StudentGradesSize - 1)
+                else if (i == studentGrades.Length - 1)
                 {
-                    if (studentGrades[i] > studentGrades[i - 1])
+                    if (studentGrades[i] >= studentGrades[i - 1])
                     {
                         Console.Write(studentGrades[i]);
                     }
