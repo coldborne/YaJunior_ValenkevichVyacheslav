453cc0a635f5eb3a01a29e572b7831391b736581
coldborne <coldborneVV@gmail.com>
2022-09-14 01:42:59 +0300

    Задача "Толковый словарь"
    Реализовал поиск значения из словаря и вывод этого значения

---
+++ Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+
+            employees.Add(1, "Программист");
+            employees.Add(2, "Тестировщик");
+            employees.Add(3, "Менеджер");
+            employees.Add(4, "Аналитик");
+            employees.Add(5, "Программист");
+
+            Console.Write("Введите сотрудника, которого хотите найти: ");
+            string word = Console.ReadLine();
+
+            if (employees.ContainsValue(word))
+            {
+                Console.WriteLine(word);
+            }
+            else
+            {
+                Console.WriteLine("Такого сотрудника нет");
+            }
+        }
+    }
+}
