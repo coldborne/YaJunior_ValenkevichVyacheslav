496ef8695860467404b04559daa7b6aeb41c4661
coldborne <coldborneVV@gmail.com>
2022-10-24 00:54:34 +0300

    AQU-12
    Создан класс Fish, внутри есть два конструктора (Один нужен для выставления цвета, в дефолтном конструкторе цвет выставляет по дефолту)
    
    Также создан методе TryReduceAge, внутри которого уменьшается возраст, если текущий возраст больше 0

---
+++ Aquarium/Fish.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aquarium
+{
+    public class Fish
+    {
+        public int Age { get; private set; }
+        public ConsoleColor Color { get; private set; }
+
+        public Fish()
+        {
+            Random random = new Random();
+
+            Age = random.Next(11);
+            Color = ConsoleColor.Gray;
+        }
+
+        public Fish(ConsoleColor color)
+        {
+            Random random = new Random();
+
+            Age = random.Next(11);
+            Color = color;
+        }
+
+        private bool TryReduceAge()
+        {
+            if (Age > 0)
+            {
+                Age--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
