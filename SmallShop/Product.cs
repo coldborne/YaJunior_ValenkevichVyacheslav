629831d7222931d931de0c340049838f18061079
coldborne <coldborneVV@gmail.com>
2023-06-15 20:13:22 +0300

    Задача "Магазин"
    Продукт
    
    1) Добавил метод ChangePrice для изменения цены у продукта
    2) Поправил выравнивание в классе

--- SmallShop/Product.cs
+++ SmallShop/Product.cs
@@ -3,7 +3,7 @@ namespace SmallShop
     public class Product
     {
         private readonly string _name;
-        
+
         public int Price { get; private set; }
         public int Weight { get; private set; }
 
@@ -14,9 +14,14 @@ namespace SmallShop
             Price = price;
         }
 
+        public void ChangePrice(int newPrice)
+        {
+            Price = newPrice;
+        }
+
         public override string ToString()
         {
-            return "Название - " + _name + "Вес - " + Weight + "Цена - " + Price;
+            return $"Название {_name}, Вес - {Weight}, Цена - {Price}";
         }
     }
 }
