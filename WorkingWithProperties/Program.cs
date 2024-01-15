2f503a1e70dcb8af8944d3d79d874f76b8cabf91
coldborne <coldborneVV@gmail.com>
2024-01-15 12:31:37 +0300

    Задача "Работа со свойствами"
    Рефакторинг:
    1) Добавил класс Drawer для отрисовки игроков
    2) В классе Player поля поменял на автореализуемые свойства
    3) В классе Player убрал использование поля _name

--- WorkingWithProperties/Program.cs
+++ WorkingWithProperties/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace WorkingWithProperties
 {
@@ -6,48 +7,64 @@ namespace WorkingWithProperties
     {
         static void Main(string[] args)
         {
-            Player slava = new Player();
-            slava.ShowInfo();
-
-            int xCoordinate = 10;
-            int yCoordinate = 12;
+            Drawer drawer = new Drawer();
+            
+            int xCoordinate = -10000;
+            int yCoordinate = -10000;
             Coordinate coordinates = new Coordinate(xCoordinate, yCoordinate);
 
-            Player suffocater = new Player(coordinates);
-            suffocater.ShowInfo();
+            Player suffocater = new Player(coordinates, "Слава");
+            Player player = new Player();
+            
+            char playerSymbol = '@';
+            char suffocaterSymbol = 'S';
+            
+            drawer.TryDrawPlayer(player, playerSymbol);
+            drawer.TryDrawPlayer(suffocater, suffocaterSymbol);
+            
+            Console.ReadKey();
+        }
+    }
+
+    public class Drawer
+    {
+        public void TryDrawPlayer(Player player, char playerSymbol)
+        {
+            if (canDraw(player.X, player.Y))
+            {
+                Console.SetCursorPosition(player.X, player.Y);
+            
+                Console.WriteLine(playerSymbol);
+            }
+        }
+
+        private bool canDraw(int x, int y)
+        {
+            if (x >= 0 && x < Console.BufferWidth && y >= 0 && y < Console.BufferHeight)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 
     public class Player
     {
-        private int _xCoordinate;
-        private int _yCoordinate;
+        private Coordinate _coordinates;
 
         public Player()
         {
-            _xCoordinate = 0;
-            _yCoordinate = 0;
+            _coordinates = new Coordinate();
         }
 
-        public Player(Coordinate coordinates)
+        public Player(Coordinate coordinates, string name)
         {
-            _xCoordinate = coordinates.X;
-            _yCoordinate = coordinates.Y;
+            _coordinates = coordinates;
         }
 
-        public string Name { get; private set; } = " ";
-
-        public void ShowInfo()
-        {
-            if (Name == " ")
-            {
-                Console.WriteLine($"X - {_xCoordinate}, Y - {_yCoordinate}");
-            }
-            else
-            {
-                Console.WriteLine($"X - {_xCoordinate}, Y - {_yCoordinate}, Имя - {Name}");
-            }
-        }
+        public int X => _coordinates.X;
+        public int Y => _coordinates.Y;
     }
     
     public class Coordinate
@@ -55,6 +72,12 @@ namespace WorkingWithProperties
         public int X { get; private set; }
         public int Y { get; private set; }
 
+        public Coordinate()
+        {
+            X = 1;
+            Y = 1;
+        }
+        
         public Coordinate(int x, int y)
         {
             X = x;
