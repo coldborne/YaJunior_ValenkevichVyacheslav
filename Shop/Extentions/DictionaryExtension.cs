732fdb894f4f2c6dca1494856ad23cb327b6a6db
coldborne <coldborneVV@gmail.com>
2024-03-01 14:43:24 +0300

    Задача "Магазин"
    Рефакторинг:
    1) Класс DictionaryExtension:
    Класс добавлен для расширения метода Add в словарь с товарами, чтобы была проверка, если такой товар уже есть, то необходимо просто увеличить количество товара, а если товара нет, то добавить его в словарь

---
+++ Shop/Extentions/DictionaryExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shop
+{
+    public static class DictionaryExtension
+    {
+        public static void Add(this Dictionary<Guid, Merchandise> merchandises, Merchandise merchandise)
+        {
+            Guid id = merchandise.Product.Id;
+
+            if (merchandises.ContainsKey(id))
+            {
+                merchandises[id].IncreaseQuantity(merchandise.Quantity);
+            }
+            else
+            {
+                merchandises.Add(merchandise.Product.Id, merchandise);
+            }
+        }
+    }
+}
