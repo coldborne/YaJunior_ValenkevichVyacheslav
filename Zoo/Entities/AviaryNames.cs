91609ba4af60908563d5314b9734971af4710377
coldborne <coldborneVV@gmail.com>
2024-09-18 19:54:27 +0300

    Реализовал класс для описания сущности вольера. Класс: Aviary Также реализовал статический класс AviaryNames для доступа к константным строкам, отвечающим за названия вальеров

---
+++ Zoo/Entities/AviaryNames.cs
@@ -0,0 +1,10 @@
+namespace Zoo.Entities
+{
+    public static class AviaryNames
+    {
+        public const string Capybara = "Капибары";
+        public const string Cat = "Кошки";
+        public const string Giraffe = "Жирафы";
+        public const string Penguin = "Пингвины";
+    }
+}
