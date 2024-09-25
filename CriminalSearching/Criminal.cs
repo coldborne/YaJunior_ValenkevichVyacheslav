efd3a0c882548a6b382565e7b4733db8ea4a811a
coldborne <coldborneVV@gmail.com>
2024-09-25 23:40:10 +0300

    Реализовал код для выполнения задачи "Поиск преступника" Criminal отвечает за каждого преступника CriminalCreator создаёт коллекцию преступников как базу данных, в которой потребуется искать преступников Поиск реализован с помощью LINQ метода расширения Where

---
+++ CriminalSearching/Criminal.cs
@@ -0,0 +1,25 @@
+namespace CriminalSearching;
+
+public class Criminal
+{
+    public Criminal(string fullName, string nationality, bool isUnderArrest, int height, int weight)
+    {
+        FullName = fullName;
+        Nationality = nationality;
+        IsUnderArrest = isUnderArrest;
+        Height = height;
+        Weight = weight;
+    }
+
+    public string FullName { get; }
+    public string Nationality { get; }
+    public bool IsUnderArrest { get; }
+    public int Height { get; }
+    public int Weight { get; }
+
+    public override string ToString()
+    {
+        return
+            $"Имя: {FullName}, национальность: {Nationality}, под арестом: {IsUnderArrest}, рост: {Height}, вес: {Weight}";
+    }
+}
