ff35ea60119c03011674ad0ff93e1e23c3765ef9
coldborne <coldborneVV@gmail.com>
2024-09-25 23:22:14 +0300

    Реализовал код для выполнения задачи "Поиск преступника" Criminal отвечает за каждого преступника CriminalCreator создаёт коллекцию преступников как базу данных, в которой потребуется искать преступников Поиск реализован с помощью LINQ метода расширения Where

---
+++ CriminalSearching/CriminalCreator.cs
@@ -0,0 +1,69 @@
+namespace CriminalSearching;
+
+public class CriminalCreator
+{
+    private static Random random = new Random();
+
+    private static List<string> firstNames = new List<string>
+    {
+        "Иван", "Алексей", "Дмитрий", "Сергей", "Михаил",
+        "Андрей", "Николай", "Павел", "Владимир", "Егор"
+    };
+
+    private static List<string> lastNames = new List<string>
+    {
+        "Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов",
+        "Попов", "Лебедев", "Козлов", "Новиков", "Морозов"
+    };
+
+    private static List<string> nationalities = new List<string>
+    {
+        "Русский", "Украинец", "Белорус", "Казах", "Армянин",
+        "Грузин", "Узбек", "Таджик", "Киргиз", "Молдаванин"
+    };
+
+    public Criminal CreateRandomCriminal()
+    {
+        string fullName = GenerateRandomFullName();
+        string nationality = GenerateRandomNationality();
+
+        int freeStateNumber = 0;
+        int underArrestStateNumber = 1;
+        int criminalStateNumber = random.Next(freeStateNumber, underArrestStateNumber + 1);
+        bool isUnderArrest = criminalStateNumber == underArrestStateNumber;
+
+        int minHeight = 170;
+        int maxHeight = 190;
+        int height = random.Next(minHeight, maxHeight + 1);
+
+        int minWeight = 70;
+        int maxWeight = 100;
+        int weight = random.Next(minWeight, maxWeight + 1);
+
+        return new Criminal(fullName, nationality, isUnderArrest, height, weight);
+    }
+
+    public List<Criminal> CreateRandomCriminals(int count)
+    {
+        List<Criminal> criminals = new List<Criminal>();
+
+        for (int i = 0; i < count; i++)
+        {
+            criminals.Add(CreateRandomCriminal());
+        }
+
+        return criminals;
+    }
+
+    private string GenerateRandomFullName()
+    {
+        string firstName = firstNames[random.Next(firstNames.Count)];
+        string lastName = lastNames[random.Next(lastNames.Count)];
+        return $"{firstName} {lastName}";
+    }
+
+    private string GenerateRandomNationality()
+    {
+        return nationalities[random.Next(nationalities.Count)];
+    }
+}
---
+++ CriminalSearching/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CriminalSearching
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CriminalCreator creator = new CriminalCreator();
+
+            int criminalsCount = 20;
+            List<Criminal> criminals = creator.CreateRandomCriminals(criminalsCount);
+
+            Console.WriteLine("База данных: ");
+
+            foreach (Criminal criminal in criminals)
+            {
+                Console.WriteLine(criminal);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Введите рост преступника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите вес преступника: ");
+            int weight = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите национальность преступника: ");
+            string nationality = Console.ReadLine();
+
+            var filteredCriminals = criminals.Where(criminal =>
+                    criminal.Height == height && criminal.Weight == weight &&
+                    criminal.Nationality.Equals(nationality, StringComparison.OrdinalIgnoreCase))
+                .Where(criminal => criminal.IsUnderArrest == false);
+
+            Console.WriteLine("\nПодходящие преступники:");
+
+            foreach (Criminal criminal in filteredCriminals)
+            {
+                Console.WriteLine(criminal);
+            }
+        }
+    }
+}
